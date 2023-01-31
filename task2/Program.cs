e797fdd94fda6e128668800bdf595e9729b30200
Gaini <color.ui>
2023-02-01 00:28:49 +0600

    Tasks from lesson 1. c#

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Int32.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int b = Int32.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+   Console.WriteLine($"Число {a} больше числа {b}"); 
+}
+else
+{
+    Console.WriteLine($"Число {b} больше числа {a}"); 
+}
---
+++ task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Int32.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int b = Int32.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье число: ");
+int c = Int32.Parse(Console.ReadLine()!);
+
+if (a > b && a > c)
+{
+    Console.WriteLine($"Число {a} - максимальное");
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine($"Число {b} - максимальное");
+}
+else
+{
+    Console.WriteLine($"Число {c} - максимальное");
+}
+
+
+Console.WriteLine("Hello, World!");
